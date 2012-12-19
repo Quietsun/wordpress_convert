6005e977f8061cd68e689c6d38e72a1f8dceb9ae
Naohisa Minagawa <info@clay-system.jp>
2012-12-19 14:02:09 +0900

    ライセンスの表記をGPLのものに変更

--- classes/WordpressConvert.php
+++ classes/WordpressConvert.php
@@ -1,19 +1,27 @@
 <?php
-/*
- * Copyright (C) 2012 NetLife Inc. All Rights Reserved.
+/**
+ * WordPress Converter for HTML Plugin
+ * 
+ * Copyright (c) 2012 NetLife Inc. All Rights Reserved.
  * http://www.netlife-web.com/
  * 
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * This work complements FLARToolkit, developed by Saqoosha as part of the Libspark project.
+ *     http://www.libspark.org/wiki/saqoosha/FLARToolKit
+ * FLARToolKit is Copyright (C)2008 Saqoosha,
+ * and is ported from NyARToolKit, which is ported from ARToolKit.
  * 
- * http://www.apache.org/licenses/LICENSE-2.0
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  * 
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 $settings = explode(",", WORDPRESS_CONVERT_SETTING_CLASSES);
